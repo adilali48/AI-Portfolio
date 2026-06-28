a70f912ac67fbd28b57d02b747960c4c2bdb451e
adilali48 <aliadilrajper11@gmail.com>
2026-06-28 15:56:36 +0500

    Added AI PDF Chatbot

--- 06-Gemini-AI-Chatbot/streamlit_app.py
+++ 06-Gemini-AI-Chatbot/streamlit_app.py
@@ -1,28 +1,68 @@
-import streamlit as st
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 
+# Load API Key
 load_dotenv()
 
 client = genai.Client(
     api_key=os.getenv("Ab8RN6L48eeuneKUFUW1Ub6PicugHJgpExCpQKTai0n0Luq7Lw")
 )
 
+# Page Settings
 st.set_page_config(
-    page_title="Adil AI Chatbot",
-    page_icon="🤖"
+    page_title="Adil AI Assistant",
+    page_icon="🤖",
+    layout="centered"
 )
 
 st.title("🤖 Adil AI Assistant")
+st.write("Ask me anything!")
 
-question = st.text_input("Ask me anything")
+# Initialize Chat History
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if st.button("Send"):
+# Display Previous Messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=question
+# Chat Input
+question = st.chat_input("Type your message...")
+
+if question:
+
+    # Show User Message
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": question
+        }
     )
 
-    st.success(response.text)
+    with st.chat_message("user"):
+        st.markdown(question)
+
+    # Loading Spinner
+    with st.spinner("🤖 AI is thinking..."):
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=question
+        )
+
+        answer = response.text
+
+    # Save AI Response
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "content": answer
+        }
+    )
+
+    # Show AI Response
+    with st.chat_message("assistant"):
+        st.markdown(answer)
